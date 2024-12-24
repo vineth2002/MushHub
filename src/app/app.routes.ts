a3245dc1a4047d99fce06a75edbd4757b04e82dc
vineth2002 <v18nallaperuma@gmail.com>
2024-12-24 18:44:02 +0530

    feat: add signup page and navbar
    - Implement initial signup page layout with basic form fields (e.g., name, email, password).
    - Add navbar(Guest Mode) with  Home,Products, About us,Contact Us and Signup.
    - Include minimal styling for both components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: '', redirectTo: '/signup', pathMatch: 'full' }
+];
